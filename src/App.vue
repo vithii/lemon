0ce25622160057643d7907993143f6f4c1ced289
Vithi <vshakthi30@gmail.com>
2023-03-20 11:42:44 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,168 @@
+<script setup>
+</script>
+
+<template>
+  <div class=" background bg-base align-middle ">
+
+    <div class="navbar flex items-center">
+      <div class="flex items-center gap-8 ">
+        <div class="h2 text-white">lemon squeezy</div>
+        <div class="flex gap-4 ">
+          <div class="p2">Pricing</div>
+          <div class="p2">Blog</div>
+          <div class="p2">Docs</div>
+          <div class="p2">@Imsqueezy</div>
+        </div>
+      </div>
+      <div class="flex items-center gap-4">
+        <div class="p2">Sign in</div>
+        <div>
+          <div class="button">Get started</div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Section 1 -->
+    <div class="container text-white mt-28">
+      <div class="flex gap-5">
+        <div class="grid gap-5">
+          <div class="h1 font-semibold">Payments, tax & subscriptions for software companies</div>
+          <div class="p2">As your merchant of record, we handle the tax compliance burden so you can focus on more revenue
+            and less
+            headache.</div>
+          <div class="button">Get started for FREE</div>
+        </div>
+        <div>
+          <img src="https://assets.website-files.com/6347244ba8d63489ba51c08e/63526aed1b59e32ca934b8ec_orders.jpg" alt="">
+        </div>
+      </div>
+      <div class="flex items-center">
+        <div class=" basis-32" >
+          <div class="p2">TRUSTED BY THOUSANDS OF COMPANIES IN 100+ COUNTRIES</div>
+        </div>
+        <div class="flex basis-40 gap-5 ">
+          <img src="https://assets.website-files.com/6347244ba8d63489ba51c08e/634d9038ef9487485a37ca3e_th.png" alt="">
+          <img src="https://assets.website-files.com/6347244ba8d63489ba51c08e/634d9074eca598605c3c3b22_landify.png" alt="">
+          <img src="https://assets.website-files.com/6347244ba8d63489ba51c08e/634d90b097320c7e0f45c0ef_fa.png" alt="">
+          <img src="https://assets.website-files.com/6347244ba8d63489ba51c08e/634d90d41137ca1b8e992a90_uipress.png" alt="">
+          <img src="https://assets.website-files.com/6347244ba8d63489ba51c08e/634d911213981b4b9d12a32b_iconic.png" alt="">
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Section 2 -->
+  <div class="container">
+    <div>WHY LEMON?</div>
+    <div class=" text-5xl">Lemon Squeezy is the all-in-one platform for running your SaaS business. Payments,
+      subscriptions, global tax compliance, fraud prevention, multi-currency support, failed payment recovery, PayPal
+      integration and more. We make running your software business easy peasy.
+    </div>
+  </div>
+
+  <!-- Section 3 -->
+  <div class="lcontainer flex">
+    <div class="basis-1/2">
+      <div class="s3points">Global tax compliance</div>
+      <div class="s3points">Global tax compliance</div>
+      <div class="s3points">Global tax compliance</div>
+      <div class="s3points">Global tax compliance</div>
+      <div class="s3points">Global tax compliance</div>
+      <div class="s3points">Global tax compliance</div>
+    </div>
+    <div class="basis-1/2">
+      <img src="https://assets.website-files.com/6347244ba8d63489ba51c08e/63517e0f41c8774735e19922_currency.jpg" alt="">
+    </div>
+  </div>
+
+  <!-- Section 4 -->
+  <div class="bg-[#f7f7f8]">
+    <div class="container">
+      <div>ECOMMERCE</div>
+      <div class="flex gap-10 ">
+        <div class=" font-semibold text-5xl basis-3/5">Create the perfect buying experience for your customers</div>
+        <div class="basis-1/5"></div>
+        <div class="p basis-3/5">Launch your secure ecommerce website and start selling software, subscriptions and
+          digital
+          downloads anywhere in minutes. Utilize no-code checkout links without worrying about design, mobile
+          optimization, or payment integrations. It really is easy-peasy.</div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Section 5 -->
+  <div class="flex">
+    <div>
+      <img src="https://assets.website-files.com/6347244ba8d63489ba51c08e/63d02f976e976873cc39e25c_ecommerce1-p-1600.jpg"
+        alt="">
+    </div>
+    <div>
+      <img src="https://assets.website-files.com/6347244ba8d63489ba51c08e/63524ff844284340137caa65_ecommerce2-p-1600.jpg"
+        alt="">
+    </div>
+  </div>
+
+  <!-- Section 6 -->
+  <div class="bg-[#f7f7f8] ">
+    <div class=" grid grid-rows-2 grid-flow-col container">
+      <div class="services">
+        <div class="h">E/1</div>
+        <div class="h3">Accept online payments</div>
+        <div class="p">Don't worry about merchant accounts and billing logic. Choose per seat, usage-based and more.
+          Customize as little or as much as you like.</div>
+      </div>
+      <div class="services">
+        <div class="h">E/1</div>
+        <div class="h3">Accept online payments</div>
+        <div class="p">Don't worry about merchant accounts and billing logic. Choose per seat, usage-based and more.
+          Customize as little or as much as you like.</div>
+      </div>
+      <div class="services">
+        <div class="h">E/1</div>
+        <div class="h3">Accept online payments</div>
+        <div class="p">Don't worry about merchant accounts and billing logic. Choose per seat, usage-based and more.
+          Customize as little or as much as you like.</div>
+      </div>
+      <div class="services">
+        <div class="h">E/1</div>
+        <div class="h3">Accept online payments</div>
+        <div class="p">Don't worry about merchant accounts and billing logic. Choose per seat, usage-based and more.
+          Customize as little or as much as you like.</div>
+      </div>
+      <div class="services">
+        <div class="h">E/1</div>
+        <div class="h3">Accept online payments</div>
+        <div class="p">Don't worry about merchant accounts and billing logic. Choose per seat, usage-based and more.
+          Customize as little or as much as you like.</div>
+      </div>
+      <div class="services">
+        <div class="h">E/1</div>
+        <div class="h3">Accept online payments</div>
+        <div class="p">Don't worry about merchant accounts and billing logic. Choose per seat, usage-based and more.
+          Customize as little or as much as you like.</div>
+      </div>
+
+
+
+
+    </div>
+
+    <div class=" bg-white">
+      <div class="container">
+        <div>MARKETING</div>
+        <div class="flex gap-10 ">
+          <div class=" font-semibold text-5xl basis-3/5">Grow your business with integrated marketing tools</div>
+          <div class="basis-1/5"></div>
+          <div class="p basis-3/5">Grow your audience, build long-lasting customer relationships, and make more sales with
+            powerful, easy-to-use marketing tools. Whether you need to offer discount codes or you want to build your
+            email list, you can do it all with Lemon Squeezy.</div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped></style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'base': '#5523e7',
+      },
+    },
+  },
+  plugins: [],
+}
